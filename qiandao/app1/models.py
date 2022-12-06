96f154a148031125523ee8169fbb7c073636b945
mighty-bear <63963147+mighty-bear@users.noreply.github.com>
2022-12-06 14:09:14 +0800

    Update models.py

--- qiandao/app1/models.py
+++ qiandao/app1/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Teacher(models.Model):
     name = models.CharField(max_length=32)
     password = models.CharField(max_length=32)
+
+    
+class mypicture(models.Model):
+    user = models.CharField(max_length=64)
+    photo = models.ImageField(upload_to='photos', default='user1.jpg')
