34f46f7f9101b08ddffe10b1c099abdf6eab9c1e
李响 <li16630358943@163.com>
2022-12-06 14:19:07 +0800

    创建models

--- qiandao/app1/models.py
+++ qiandao/app1/models.py
@@ -2,8 +2,39 @@ from django.db import models
 
 # Create your models here.
 class Teacher(models.Model):
+    user = models.CharField(max_length=32)
     name = models.CharField(max_length=32)
+    teacherNo = models.CharField(max_length=32)
     password = models.CharField(max_length=32)
+    courseNo = models.CharField(max_length=32)# 所教课程
+    classNo = models.CharField(max_length=32)# 开班号
+
+class Student(models.Model):
+    name = models.CharField(max_length=32)
+    studentNo = models.CharField(max_length=32)
+    face = ?
+
+class Course(models.Model): # 课程
+    courseNo = models.CharField(max_length=32)
+    courseName = models.CharField(max_length=32)
+    grade = models.FloatField()
+
+class Class(models.Model): # 开课版
+    teacherNo = models.CharField(max_length=32)
+    courseNo = models.CharField(max_length=32) # 课程号
+    classNo = models.CharField(max_length=32) # 课程班号
+    studentNo = models.CharField(max_length=32)
+    qianDaoSum = models.IntegerField()
+class QianDao(models.Model):
+    qianDaoName = models.CharField(max_length=32)
+    courseNo = models.CharField(max_length=32)
+
+
+class QianDaoMessage(models.Model):
+    studentNo = models.CharField(max_length=32)
+    time = models.TimeField()
+
+
 
     
 class mypicture(models.Model):
