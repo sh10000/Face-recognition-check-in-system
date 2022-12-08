d154058887155a376df16082be174c8fba5499e8
李响 <li16630358943@163.com>
2022-12-08 18:34:31 +0800

    Changes

--- qiandao/app1/models.py
+++ qiandao/app1/models.py
@@ -4,33 +4,31 @@ from django.db import models
 class Teacher(models.Model):
     user = models.CharField(max_length=32)
     name = models.CharField(max_length=32)
-    teacherNo = models.CharField(max_length=32)
+    teacherNo = models.CharField(max_length=32, primary_key=True)
     password = models.CharField(max_length=32)
-    courseNo = models.CharField(max_length=32)# 所教课程
-    classNo = models.CharField(max_length=32)# 开班号
 
 class Student(models.Model):
     name = models.CharField(max_length=32)
-    studentNo = models.CharField(max_length=32)
+    studentNo = models.CharField(max_length=32, primary_key=True)
     photo = models.ImageField(upload_to='photos', default='user1.jpg')
 class Course(models.Model): # 课程
-    courseNo = models.CharField(max_length=32)
+    courseNo = models.CharField(max_length=32, primary_key=True)
     courseName = models.CharField(max_length=32)
     grade = models.FloatField()
 
 class Class(models.Model): # 开课版
-    teacherNo = models.CharField(max_length=32)
-    courseNo = models.CharField(max_length=32) # 课程号
-    classNo = models.CharField(max_length=32) # 课程班号
-    studentNo = models.CharField(max_length=32)
-    qianDaoSum = models.IntegerField()
+    teacherNo = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    courseNo = models.ForeignKey(Course, on_delete=models.CASCADE)  # 课程号
+    classNo = models.CharField(max_length=32, primary_key=True) # 课程班号
+    students = models.ManyToManyField(Student)
 class QianDao(models.Model):
     qianDaoName = models.CharField(max_length=32)
-    courseNo = models.CharField(max_length=32)
+    courseName = models.CharField(max_length=32)
+    class1 = models.ForeignKey(Class, on_delete=models.CASCADE)
 
 
 class QianDaoMessage(models.Model):
-    studentNo = models.CharField(max_length=32)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
     time = models.TimeField()
 
 
