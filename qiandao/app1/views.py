71f63ee6e882fc0f26cb24d92c129a906474c6f7
权泽睿 <1727954273@qq.com>
2023-01-11 16:01:48 +0800

    1

--- qiandao/app1/views.py
+++ qiandao/app1/views.py
@@ -437,8 +437,10 @@ def signinfo(request):
     stuName = request.get_signed_cookie("username", salt="dsb")
     classNo=request.GET.get('classNo')
     cursor = connection.cursor()
-    sql ="select classNo,b.courseName,QTime from renLianShiBie1.app1_class a,renLianShiBie1.app1_course b,renLianShiBie1.app1_stuqiandao c,renLianShiBie1.app1_qiandao d where a.course_id=b.courseNo and c.QianDaoId_id=d.id and d.class1_id =a.classNo and studentNo_id="+stuName+" and classNo="+classNo+" order by c.id DESC"
+    sql ="select classNo,b.courseName,QTime,status from renLianShiBie1.app1_class a,renLianShiBie1.app1_course b,renLianShiBie1.app1_stuqiandao c,renLianShiBie1.app1_qiandao d where a.course_id=b.courseNo and c.QianDaoId_id=d.id and d.class1_id =a.classNo and studentNo_id="+stuName+" and classNo="+classNo+" order by c.id DESC"
     cursor.execute(sql)
     res = cursor.fetchall()
-    a="已签到"
-    return render(request, "Student/SignInfo.html",{"stuName": stuName,"n1":a,"res":res})
+    if res[0][3]==0:
+        return render(request, "Student/SignInfo.html",{"stuName": stuName,"n1":"未签到","res":res})
+    else:
+        return render(request, "Student/SignInfo.html",{"stuName": stuName,"n1":"已签到","res":res})
