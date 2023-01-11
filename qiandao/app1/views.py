228331b92de95214c67e4beecf9f6ca60fe39648
权泽睿 <1727954273@qq.com>
2023-01-11 16:29:14 +0800

    1

--- qiandao/app1/views.py
+++ qiandao/app1/views.py
@@ -443,8 +443,6 @@ def signinfo(request):
     sql ="select classNo,b.courseName,QTime,status from renLianShiBie1.app1_class a,renLianShiBie1.app1_course b,renLianShiBie1.app1_stuqiandao c,renLianShiBie1.app1_qiandao d where a.course_id=b.courseNo and c.QianDaoId_id=d.id and d.class1_id =a.classNo and studentNo_id="+stuName+" and classNo="+classNo+" order by c.id DESC"
     cursor.execute(sql)
     res = cursor.fetchall()  
-    return render(request, "Student/SignInfo.html",{"stuName": stuName,"res":res})
-    res = cursor.fetchall()
     if res[0][3]==0:
         return render(request, "Student/SignInfo.html",{"stuName": stuName,"n1":"未签到","res":res})
     else:
